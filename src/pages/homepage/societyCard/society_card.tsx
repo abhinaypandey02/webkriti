64de18afefffaae35e4cc16983c50f9708dea4c3
Abhinay Pandey <abhinaypandey02@gmail.com>
2021-04-11 10:56:47 +0530

    minor bug fixes with organiser's permission

--- src/pages/homepage/societyCard/society_card.tsx
+++ src/pages/homepage/societyCard/society_card.tsx
@@ -7,14 +7,14 @@ export default function SocietyCard({society,index}:{society:Society,index:numbe
     const [theme]=useTheme();//global state for theme
     return (
         <Card onClick={()=>history.push('/'+society.slug)} className={'my-3 pointer-on-hover '+(index%2===0?"text-left ":"text-right ")+(theme==="dark"?"bg-dark border-light":"shadow-2")}>
-            <Card.Body className={'d-flex '+(index%2===0?"justify-content-start":"justify-content-end")}>
-                {index%2===0&&society.gallery.length>0&&<img height={200} src={society.gallery[0]} alt={society.name}/>}
+            <Card.Body className={'d-flex flex-wrap flex-md-nowrap '+(index%2===0?"justify-content-start":"justify-content-end")}>
+                {index%2===0&&society.gallery.length>0&&<img className="m-3 m-md-0" height={200} src={society.gallery[0]} alt={society.name}/>}
                 <div className={'mx-4 '+(theme==="dark"&&"text-white")}>
                     <h1>{society.name}</h1>
                     <h4>{society.bio}</h4>
                     <small className={(theme==="dark"?"text-white":"text-muted")}>{society.description}</small>
                 </div>
-                {index%2!==0&&society.gallery.length>0&&<img height={200} src={society.gallery[0]} alt={society.name}/>}
+                {index%2!==0&&society.gallery.length>0&&<img className="m-3 m-md-0" height={200} src={society.gallery[0]} alt={society.name}/>}
 
 
             </Card.Body>
--- src/pages/societyPage/eventCard/event_card.tsx
+++ src/pages/societyPage/eventCard/event_card.tsx
@@ -3,9 +3,11 @@ import EventInterface from "../../../interfaces/event";
 import {addEvent} from "../../../utilities/firebase/firestore";
 import Society from "../../../interfaces/society";
 import {useTheme} from "../../../contexts/theme_context";
+import {useUser} from "../../../contexts/user_context";
 
 export default function EventCard({society,event,onUpdateEvent}:{society:Society,event:EventInterface,onUpdateEvent:()=>void}) {
     const date=new Date(event.time);//js date of the ISO String
+    const [user]=useUser();
     const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
         "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"
     ];
@@ -20,7 +22,7 @@ export default function EventCard({society,event,onUpdateEvent}:{society:Society
             <Card.Body className={'text-left'}>
                 <div className={'h3 d-flex justify-content-between'}>
                     {event.title}
-                    <Button style={{width:40,height:40}} variant={'danger'} className={'rounded-circle'} onClick={onDelete}>x</Button>
+                    {user&&<Button style={{width:40,height:40}} variant={'danger'} className={'rounded-circle'} onClick={onDelete}>x</Button>}
                 </div>
                 <small>{timeString}</small>
 
